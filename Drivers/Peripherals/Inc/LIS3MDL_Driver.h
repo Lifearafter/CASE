97d16de9a461031434bdc61ca5d23bdb922b12a6
Muhammad Zaid <mxz190024@utdallas.edu>
2024-03-26 15:39:01 -0500

    feat: add all kicad schematics for the project, seperated by subsystem ie Flight, Power, Comms

--- Drivers/Peripherals/Inc/LIS3MDL_Driver.h
+++ Drivers/Peripherals/Inc/LIS3MDL_Driver.h
@@ -12,10 +12,12 @@ struct SPI_Module
 };
 
 void LIS3MDL_Init(void);
+void LIS3MDL_Config(void);
+
+HAL_StatusTypeDef LIS3MDL_Check(void);
 HAL_StatusTypeDef LIS3MDL_ReadReg(uint8_t register_address, uint8_t *data, uint16_t size);
 HAL_StatusTypeDef LIS3MDL_WriteReg(uint8_t register_address, uint8_t *data, uint16_t size);
-void LIS3MDL_Config(void);
-HAL_StatusTypeDef LIS3MDL_Check(void);
+
 void LIS3MDL_ReadMag(int16_t *x, int16_t *y, int16_t *z);
 
 #endif /* LIS3MDL_DRIVER_H */
